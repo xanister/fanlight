30ded7a603b5567e46cdb36bb9c5b8aa797e3757
Nicholas Frees <xanister@inkintosh.lan>
2018-06-17 15:13:43 -0400

    inc

--- src/fanlight.js
+++ src/fanlight.js
@@ -4,14 +4,18 @@ import Carriage from "carriage";
 
 export default class Fanlight {
   constructor() {
-    this.aperture = new Aperture();
-    this.biosphere = new Biosphere();
-    this.carriage = new Carriage();
+    this.aperture = new Aperture();     // Manages view
+    this.biosphere = new Biosphere();   // Manages world
+    this.carriage = new Carriage();     // Manages user input
 
     this.run();
   }
 
   run() {
+    // Send any user input to the player
+    this.biosphere.player.inputs = this.carriage.serialize();
+
+    // Send any visible items to the view
     this.aperture.items = this.biosphere.player.serializeView();
   
     window.requestAnimationFrame(() => this.run());
