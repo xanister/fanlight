40d9548ca9a7083d951642fff05dd21bd44d5bed
Nicholas Frees <xanister@inkintosh.lan>
2018-06-17 15:03:02 -0400

    inc

--- src/fanlight.js
+++ src/fanlight.js
@@ -1,9 +1,17 @@
 import Aperture from "aperture";
 import Biosphere from "biosphere";
+import Carriage from "carriage";
 
 export default class Fanlight {
   constructor() {
     this.aperture = new Aperture();
     this.biosphere = new Biosphere();
+    this.carriage = new Carriage();
+
+    window.requestAnimationFrame(() => run());
+  }
+
+  run() {
+    this.aperture.items = this.biosphere.player.serializeView();
   }
 }
