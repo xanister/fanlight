0b33bc75bd3242a89b76f5628f2656d0b57e4f54
Nicholas Frees <xanister@inkintosh.lan>
2018-06-17 14:06:19 -0400

    inc

--- src/fanlight.js
+++ src/fanlight.js
@@ -1,7 +1,9 @@
 import Aperture from "aperture";
+import Biosphere from "biosphere";
 
 export default class Fanlight {
   constructor() {
     this.aperture = new Aperture();
+    this.biosphere = new Biosphere();
   }
 }
